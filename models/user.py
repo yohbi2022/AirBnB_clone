f6c9f7e5cc427c87e91ebdf3072ee0b564ffd35f
Your Nameyohbi2022git config user.name git clone https://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@gitclearroot@c174ab32603f:/alx-pre_course# git config --global user.name Your <ybirara@yahoo.com>
2023-05-16 09:06:35 -0700

    update

--- models/user.py
+++ models/user.py
@@ -1,19 +1,58 @@
 #!/usr/bin/python3
-"""Implements the user's model"""
-from models.base_model import BaseModel
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
-class User(BaseModel):
-    """
-    Inherits from the BaseModel class and add user's functionalities
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    Args:
-        email (str): the email of the user
-        password (str): the password of the user
-        first_name (str): the first name of the user
-        last_name (str): the last name of the user
-    """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+        @property
+        def places(self):
+            """getter attribute returns the list of Place instances"""
+            from models.place import Place
+            place_list = []
+            all_places = models.storage.all(Place)
+            for place in all_places.values():
+                if place.user_id == self.id:
+                    place_list.append(place)
+            return place_list
+
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.user_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, key, value):
+        """Overrides User setattr function to encrypt password value"""
+        if key == 'password':
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(key, value)
